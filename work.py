2d7bd831a34e5a895600188c698936bbc96ed70d
clyde5649 <clyde.bledsoe@yahoo.com>
2019-01-28 08:52:48 -0600

    work

---
+++ work.py
@@ -0,0 +1,15 @@
+def exercise7()
+
+stopval = int(input("enter a number; "))
+
+for fbnum in range(1(stopval + 1 ));
+    if (fbnum % 3 ==0);
+    print("fuzz")
+elif (fbnum % 5 == 0);
+   print("buzz")
+
+elif(fbnum % 3 == 0 and fbnum % 5 == 0);
+print("fizzbuzz")
+
+else:
+   print(fbnum)
